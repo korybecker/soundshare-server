a81994fa17c3cca39ec89341a20b859447dc0bee
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-23 18:30:36 -0500

    added likes attribute

--- models/soundModel.js
+++ models/soundModel.js
@@ -1,17 +1,18 @@
 //    Sound Model
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const soundSchema = new mongoose.Schema(
-	{
-		title: { type: String, required: true },
-		url: { type: String, required: true },
-		description: { type: String },
-		uploadedBy: { type: String },
-	},
-	{
-		timestamps: true,
-	}
+    {
+        title: { type: String, required: true },
+        url: { type: String, required: true },
+        description: { type: String },
+        uploadedBy: { type: String },
+        likes: { type: Number },
+    },
+    {
+        timestamps: true,
+    }
 );
 
-module.exports = mongoose.model('Sound', soundSchema);
+module.exports = mongoose.model("Sound", soundSchema);
