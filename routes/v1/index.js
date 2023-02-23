b37b959afb40c4d3fadced160cc83e47a6bc205b
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-23 18:33:41 -0500

    changes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,22 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const usersRoute = require('./users');
-const soundsRoute = require('./sounds');
+const usersRoute = require("./users");
+const soundsRoute = require("./sounds");
+const likesRoute = require("./likes");
 
 // const requireAuth = require('../middleware/requireAuth');
 
 module.exports = (params) => {
-	router.get('/', (req, res) => {
-		res.send('<h1>Home Page</h1>');
-	});
+    router.get("/", (req, res) => {
+        res.send("<h1>Home Page</h1>");
+    });
 
-	// // add require auth middleware to router
-	// router.use(requireAuth);
+    // // add require auth middleware to router
+    // router.use(requireAuth);
 
-	// hook all routes to router
-	router.use('/user', usersRoute(params.userService));
-	router.use('/sound', soundsRoute(params.soundService));
+    // hook all routes to router
+    router.use("/user", usersRoute(params.userService));
+    router.use("/sound", soundsRoute(params.soundService));
+    router.use("/like", likesRoute(params.likeService));
 
-	return router;
+    return router;
 };
