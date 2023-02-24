9891bdc9eca9d71f104d7aa8a6f67c1316b93316
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-23 22:52:46 -0500

    added hasSounds attribute, removed validator

--- models/userModel.js
+++ models/userModel.js
@@ -1,64 +1,64 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
-	{
-		email: { type: String, required: true, unique: true },
-		name: { type: String, required: true },
-		username: { type: String, required: true, unique: true },
-		password: { type: String, required: true },
-		pfpUrl: { type: String },
-	},
-	{
-		timestamps: true,
-	}
+    {
+        email: { type: String, required: true, unique: true },
+        name: { type: String, required: true },
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        pfpUrl: { type: String },
+        hasSounds: { type: Boolean, default: false },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 // static login method
 userSchema.statics.login = async function (email, password) {
-	const user = await this.findOne({ email });
+    const user = await this.findOne({ email });
 
-	if (!user) {
-		throw Error('Incorrect email');
-	}
-	const match = await bcrypt.compare(password, user.password);
-	if (!match) {
-		throw Error('Incorrect password');
-	}
-	return user;
+    if (!user) {
+        throw Error("Incorrect email");
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        throw Error("Incorrect password");
+    }
+    return user;
 };
 
 // static signup method
 userSchema.statics.signup = async function (
-	email,
-	name,
-	username,
-	password,
-	pfpUrl
+    email,
+    name,
+    username,
+    password,
+    pfpUrl
 ) {
-	// check if email exists
-	const exists = await this.findOne({ email });
+    // check if email exists
+    const exists = await this.findOne({ email });
 
-	if (exists) {
-		throw Error('Email already in use');
-	}
+    if (exists) {
+        throw Error("Email already in use");
+    }
 
-	// hash password with bcrypt
-	// salt - add chars to end of password before encrypt (same passwords, different salts)
-	const salt = await bcrypt.genSalt(10);
+    // hash password with bcrypt
+    // salt - add chars to end of password before encrypt (same passwords, different salts)
+    const salt = await bcrypt.genSalt(10);
 
-	const hashedPassword = await bcrypt.hash(password, salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-	const user = await this.create({
-		email,
-		name,
-		username,
-		password: hashedPassword,
-		pfpUrl,
-	});
+    const user = await this.create({
+        email,
+        name,
+        username,
+        password: hashedPassword,
+        pfpUrl,
+    });
 
-	return user;
+    return user;
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
