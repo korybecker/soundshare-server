7006fb9b8610a4b9a56788f8f5113d18f6e10d6e
Kory Becker <68331399+korybecker@users.noreply.github.com>
2023-02-24 20:14:58 -0500

    added multer middleware

--- routes/v1/users/index.js
+++ routes/v1/users/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer();
 
 const requireAuth = require("../../../middleware/requireAuth");
 
 module.exports = (userService) => {
     router
         .post("/login", userService.loginUser)
-        .post("/signup", userService.signupUser)
+        .post("/signup", upload.single("file"), userService.signupUser)
         .get("/login", (req, res) => {
             res.send("joe mama");
         })
